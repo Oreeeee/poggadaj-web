c5ff7050fdebf1793551fbee27de4a4dba44d942
Oreeeee <oree3ee@gmail.com>
2024-10-18 02:57:11 +0200

    Add buttons to the header

--- src/Header.svelte
+++ src/Header.svelte
@@ -6,3 +6,10 @@
     <h1>poGGadaj</h1>
     <h2>{$_('header-subtext')}</h2>
 </hgroup>
+
+<div>
+    <button>{$_('home')}</button>
+    <button>{$_('download')}</button>
+    <button>{$_('account')}</button>
+    <button>{$_('stats')}</button>
+</div>

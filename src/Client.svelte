29e97dfed6be562da02ff97290195c62a3412d23
Oreeeee <oree3ee@gmail.com>
2024-10-18 05:07:02 +0200

    Warn about specific issues with clients

--- src/Client.svelte
+++ src/Client.svelte
@@ -2,12 +2,32 @@
     export let name;
     export let description;
     export let imageUrl;
+    export let noSSL;
+    export let insecureHash;
+    export let modernWinProblems;
+
+    import { _ } from "svelte-i18n"
+    import Warning from "./Warning.svelte";
 </script>
 
 <div class="client">
     <h1 class="client-name">{name}</h1>
     <div class="column">
         <p class="client-description">{description}</p>
+
+        {#if noSSL}
+        <Warning wType="serious" message={$_("no-ssl")} />
+        <br>
+        {/if}
+
+        {#if insecureHash}
+        <Warning wType="warn" message={$_("insecure-hash")} />
+        <br>
+        {/if}
+
+        {#if modernWinProblems}
+        <Warning wType="info" message={$_("modern-win-problems")} />
+        {/if}
     </div>
     <div class="column">
         <img src={imageUrl} alt="Screenshot of {name} connected to poGGadaj">
--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -5,9 +5,9 @@
 
 <h1>{$_("patched-clients")}</h1>
 <div id="client-box">
-    <Client name="Gadu-Gadu 6.1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nibh arcu, aliquam in semper in, mattis in nibh. Duis cursus." imageUrl="gg61.png" />
+    <Client name="Gadu-Gadu 6.1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nibh arcu, aliquam in semper in, mattis in nibh. Duis cursus." imageUrl="gg61.png" noSSL insecureHash modernWinProblems />
     <br>
-    <Client name="Gadu-Gadu 6.1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nibh arcu, aliquam in semper in, mattis in nibh. Duis cursus." imageUrl="gg61.png" />
+    <Client name="Gadu-Gadu 6.1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nibh arcu, aliquam in semper in, mattis in nibh. Duis cursus." imageUrl="gg61.png" noSSL insecureHash modernWinProblems />
 </div>
 <h1>{$_("auto-patching")}</h1>
 <h1>{$_("unpatched")}</h1>

483faef210ea55c972c11f98542e0f4dd8527b63
Oreeeee <oree3ee@gmail.com>
2024-10-18 22:40:55 +0200

    Add info labels for downloads

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -1,17 +1,22 @@
 <script>
     import { _ } from 'svelte-i18n'
     import Client from "../../Client.svelte";
+    import Warning from '../../Warning.svelte';
 </script>
 
 <h1>{$_("patched-clients")}</h1>
+<Warning wType="info" message={$_("patched-clients-note")} />
 <div id="client-box">
     <Client name="Gadu-Gadu 6.1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nibh arcu, aliquam in semper in, mattis in nibh. Duis cursus." imageUrl="gg61.png" noSSL insecureHash modernWinProblems />
     <br>
     <Client name="Gadu-Gadu 6.1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nibh arcu, aliquam in semper in, mattis in nibh. Duis cursus." imageUrl="gg61.png" noSSL insecureHash modernWinProblems />
 </div>
 <h1>{$_("auto-patching")}</h1>
+<Warning wType="info" message="Work in progress!" />
 <h1>{$_("unpatched")}</h1>
+<Warning wType="warn" message={$_("unpatched-clients-note")} />
 <h1>{$_("manual")}</h1>
+<Warning wType="serious" message={$_("manual-patching-note")} />
 
 <style>
     h1 {

662f31137b05b5d087aab55bd2dd8da2831a9cf0
Oreeeee <oree3ee@gmail.com>
2024-10-18 04:16:35 +0200

    Add downloads page

---
+++ src/Client.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let name;
+    export let description;
+    export let imageUrl;
+</script>
+
+<div class="client">
+    <h1 class="client-name">{name}</h1>
+    <div class="column">
+        <p class="client-description">{description}</p>
+    </div>
+    <div class="column">
+        <img src={imageUrl} alt="Screenshot of {name} connected to poGGadaj">
+    </div>
+</div>
+
+<style>
+    .client {
+        padding: 10px;
+        background-color: darkgrey;
+        height: auto;
+        overflow-y: auto;
+        border: 2px solid black;
+    }
+
+    .client-name {
+        text-align: left;
+        font-family: 'JetBrains Mono Medium';
+    }
+
+    .client-description {
+        font-family: 'Lato Regular';
+    }
+</style>
---
+++ src/routes/download/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { _ } from 'svelte-i18n'
+    import Client from "../../Client.svelte";
+</script>
+
+<h1>{$_("patched-clients")}</h1>
+<div id="client-box">
+    <Client name="Gadu-Gadu 6.1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nibh arcu, aliquam in semper in, mattis in nibh. Duis cursus." imageUrl="gg61.png" />
+    <br>
+    <Client name="Gadu-Gadu 6.1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nibh arcu, aliquam in semper in, mattis in nibh. Duis cursus." imageUrl="gg61.png" />
+</div>
+<h1>{$_("auto-patching")}</h1>
+<h1>{$_("unpatched")}</h1>
+<h1>{$_("manual")}</h1>
+
+<style>
+    h1 {
+        font-family: 'JetBrains Mono Medium';
+        font-weight: 400;
+    }
+
+    #client-box {
+        padding: 10px;
+    }
+</style>

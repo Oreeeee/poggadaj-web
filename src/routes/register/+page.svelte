fcb3eea502d64fd0b76169b44e3b9231857df451
Oreeeee <oree3ee@gmail.com>
2024-10-21 00:46:02 +0200

    Add a note when registering

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -3,4 +3,6 @@
 </script>
 
 <h1>Register</h1>
+<p>Note: the password you choose here is for the website, not for the client</p>
+<p>You will have to set you client password on the website (they are separate for security reasons)</p>
 <LoginForm usage="register" />

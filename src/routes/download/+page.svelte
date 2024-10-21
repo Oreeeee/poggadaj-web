0525539540c00af2f6afed9002436207ed920dcd
Oreeeee <oree3ee@gmail.com>
2024-10-21 19:44:54 +0200

    Update images

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -7,16 +7,13 @@
 <h1>{$_("patched-clients")}</h1>
 <Warning wType="info" message={$_("patched-clients-note")} />
 <div id="client-box">
-    <!-- TODO: Replace image for GG 6.0 -->
-    <Client name="Gadu-Gadu 6.0" description={$_("gg60-description")} imageUrl="gg61.png" noSSL insecureHash modernWinProblems />
+    <Client name="Gadu-Gadu 6.0" description={$_("gg60-description")} imageUrl="gg60.png" noSSL insecureHash modernWinProblems />
     <br>
-    <!-- TODO: Replace with a Windows XP screenshot -->
     <Client name="Gadu-Gadu 6.1" description={$_("gg61-description")} imageUrl="gg61.png" noSSL insecureHash modernWinProblems />
     <br>
-    <!-- TODO: Yeah, that too -->
-    <Client name="Gadu-Gadu 7.0" description={$_("gg70-description")} imageUrl="gg61.png" noSSL insecureHash modernWinProblems />
+    <Client name="Gadu-Gadu 7.0" description={$_("gg70-description")} imageUrl="gg70.png" noSSL insecureHash modernWinProblems />
     <br>
-    <Client name="Gadu-Gadu 7.1" description={$_("gg71-description")} imageUrl="gg61.png" noSSL insecureHash modernWinProblems />
+    <Client name="Gadu-Gadu 7.1" description={$_("gg71-description")} imageUrl="gg71.png" noSSL insecureHash modernWinProblems />
 </div>
 <!--
 <h1>{$_("auto-patching")}</h1>

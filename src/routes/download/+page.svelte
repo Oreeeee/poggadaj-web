c4349cb2729fd851be08ffd061afb2491927ea24
Oreeeee <oree3ee@gmail.com>
2024-11-11 15:47:15 +0100

    Update downloads page

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -16,14 +16,14 @@
     <Client name="Gadu-Gadu 7.1" description={$_("gg71-description")} imageUrl="gg71.png" downloadUrl="/dl/gg71-poggadajv1.0.zip" noSSL insecureHash modernWinProblems />
 </div>
 
-<h1>{$_("patched-clients")}</h1>
+<h1>{$_("unpatched-clients")}</h1>
 <Warning wType="info" message={$_("unpatched-guide")} />
 <div id="client-box">
-    <Client name="Gadu-Gadu 7.5" description="Klient, w którym poprawiono bezpieczeństwo haseł i naprawiono wsparcie dla nowszych systemów operacyjnych" imageUrl="gg71.png" downloadUrl="/dl/gg71-poggadajv1.0.zip" noSSL />
+    <Client name="Gadu-Gadu 7.5" description={$_("gg75-description")} imageUrl="gg75.png" downloadUrl="/dl/gg75.zip" noSSL />
     <br>
-    <Client name="Gadu-Gadu 7.6" description={$_("gg71-description")} imageUrl="gg71.png" downloadUrl="/dl/gg71-poggadajv1.0.zip" noSSL />
+    <Client name="Gadu-Gadu 7.6" description={$_("gg71-description")} imageUrl="gg76.png" downloadUrl="/dl/gg76.zip" noSSL />
     <br>
-    <Client name="Gadu-Gadu 7.7" description={$_("gg71-description")} imageUrl="gg71.png" downloadUrl="/dl/gg71-poggadajv1.0.zip" noSSL />
+    <Client name="Gadu-Gadu 7.7" description={$_("gg77-description")} imageUrl="gg77.png" downloadUrl="/dl/gg77.zip" noSSL />
 </div>
 <!--
 <h1>{$_("auto-patching")}</h1>
